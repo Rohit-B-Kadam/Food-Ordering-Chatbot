79800ac371d39288abff75e0ee253ca18b679e6e
rohit-b-kadam <rohitkadam1407@gmail.com>
2020-07-07 12:38:33 +0530

    handle all punctuation.

--- model/training/create_slot_filler_dataset.py
+++ model/training/create_slot_filler_dataset.py
@@ -1,37 +1,43 @@
 import json
-import os
+import pandas as pd
+import spacy
 from path_manager import PATHS
 
 MAIN_DATASET_PATHS = PATHS["dataset"]
-UTTERANCES_FILE = "model/training/training_data/utterances.txt"
-BIO_TAGS_FILE = "model/training/training_data/bio_tags.txt"
-INTENT_FILE = "model/training/training_data/intent.txt"
+UTTERANCES_FILE = PATHS["slot_tagger_dataset"]["utterances"]
+BIO_TAGS_FILE = PATHS["slot_tagger_dataset"]["bio_tags"]
+INTENT_FILE = PATHS["slot_tagger_dataset"]["intent"]
 
-UTTERANCES = ""
-BIO_TAGS = ""
-INTENT = ""
+UTTERANCES = []
+BIO_TAGS = []
+INTENT = []
+
+NLP = spacy.load('en_core_web_sm')
+DFLIST = []
 
 
 def write_in_file():
     with open(UTTERANCES_FILE, "w") as fd:
-        fd.write(UTTERANCES)
+        fd.write("\n".join(UTTERANCES))
 
     with open(BIO_TAGS_FILE, "w") as fd:
-        fd.write(BIO_TAGS)
+        fd.write("\n".join(BIO_TAGS))
 
     with open(INTENT_FILE, "w") as fd:
-        fd.write(INTENT)
+        fd.write("\n".join(INTENT))
 
 
-def save_entries(text, bio_tags, intent ):
+def save_entries(text: str, bio_tags: str, intent: str):
     global UTTERANCES, BIO_TAGS, INTENT
-    UTTERANCES += text + "\n"
-    BIO_TAGS += bio_tags + '\n'
-    INTENT += intent + '\n'
+    UTTERANCES.append(text)
+    BIO_TAGS.append(bio_tags)
+    INTENT.append(intent)
 
 
 def get_tag(index, segments):
     tag = "O"  # default value
+    if not segments:
+        return tag
 
     for segment in segments:
         annotation = segment["annotations"][0]["name"]
@@ -42,15 +48,24 @@ def get_tag(index, segments):
     return tag
 
 
-def bio_tagger(text: str, segments: []) -> str:
-    tokens = text.split(" ")
-    tags = []
+def bio_tagger(text: str, segments: [] = None):
+    doc = NLP(text)
     index = 0
-    for token in tokens:
+    tags = []
+    tokens = []
+    for token in doc:
         tag = get_tag(index, segments)
         tags.append(tag)
-        index += len(token) + 1  # plus 1 for space
-    return " ".join(tags)   # return string
+        tokens.append(token.text)
+        index += len(token.text)
+        if index != len(text) and text[index] == " ":  # if there is space
+            index += 1
+
+    # DFLIST.append(tokens)
+    # DFLIST.append(tags)
+    tokens = " ".join(tokens)
+    tags = " ".join(tags)
+    return tokens, tags
 
 
 def extract_intent(instruction_id: str):
@@ -69,14 +84,27 @@ def create_bio_dataset():
             text = utterance["text"]
             segments = utterance.get("segments")
             if segments:
-                tags = bio_tagger(text, segments)
-                save_entries(text, tags, intent)
+                tokens, tags = bio_tagger(text, segments)
             else:
-                print("No segment")
+                tokens, tags = bio_tagger(text)
+            save_entries(tokens, tags, intent)
 
+    # df = pd.DataFrame(DFLIST)
+    # df.to_csv("model/training/training_data/check.csv")
     # write to the file
     write_in_file()
 
 
-if __name__ == "__main__":
+# def check_spacy():
+#
+#     doc = nlp("This's great")
+#     print([token.text for token in doc])
+#     print([token.lemma_ for token in doc])
+
+
+def main():
     create_bio_dataset()
+
+
+if __name__ == "__main__":
+    main()
--- path_manager.py
+++ path_manager.py
@@ -1,4 +1,9 @@
 PATHS = {
     "dataset": "model/data/food-ordering-dataset.json",
     "ontology": "model/data/ontology.json",
+    "slot_tagger_dataset": {
+        "utterances": "model/training/training_data/utterances.txt",
+        "bio_tags": "model/training/training_data/bio_tags.txt",
+        "intent": "model/training/training_data/intent.txt"
+    }
 }
